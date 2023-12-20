fda4827e45cc96d164082a3ddfbe50005f5e4c75
Marianna <maryanarez@gmail.com>
2023-12-20 18:12:10 +0100

    My-first-react-app

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './Header.jsx'
+import Food from './Food.jsx'
+import Footer from './Footer.jsx'
+
+
+/* --> To serve the root <-- */
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Food/>
+    <Footer/>
+    </>
+  );
+}
+export default App
